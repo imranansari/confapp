c2d1ed71f1ee6e5741a1178e9553afade8f03bac
iansari <imran.iansari@gmail.com>
2012-02-18 07:19:09 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+gem 'rails', '3.1.3'
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+gem 'jquery-rails'
+gem "rspec-rails", ">= 2.8.1", :group => [:development, :test]
+gem "database_cleaner", ">= 0.7.1", :group => :test
+gem "mongoid-rspec", ">= 1.4.4", :group => :test
+gem "bson_ext", ">= 1.3.1"
+gem "mongoid", ">= 2.4.3"
+gem "devise", ">= 2.0.0"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Confapp::Application.config.secret_token = '3ee5d08023862e277b07a7b1644a42b0993a8eddbb3c116d968b3c5628e42c5652cc4cf2e4766bd08921cce36b91e7329ece6314820e17bfae46a378f4be1c11'
