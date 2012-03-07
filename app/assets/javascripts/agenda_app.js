c2030371fcbed4115d99e7a371c08cc102211628
iansari <imran.iansari@gmail.com>
2012-03-06 19:14:58 -0500

    consolelog fix

--- app/assets/javascripts/agenda_app.js
+++ app/assets/javascripts/agenda_app.js
@@ -1,7 +1,7 @@
 require(["order!jquery",
     "order!livequery",
     "order!rails",
-    "order!viewporter",
+    //"order!viewporter",
     "order!handlebars",
     "order!backbone",
     "order!modelbinding",
@@ -10,8 +10,4 @@ require(["order!jquery",
     "views/agendaAppView"
 ], function (AgendaAppView) {
     var agendaAppView = new AgendaAppView();
-
-    var debugging = false; // or true
-    if (typeof console == "undefined") var console = { log: function() {} };
-    else if (!debugging || typeof console.log == "undefined") console.log = function() {};
 });
